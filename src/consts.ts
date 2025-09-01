1b738c0f76c53ca6a09ac76398be5d0c4dbd5ea1
RobynT98 <50335159+RobynT98@users.noreply.github.com>
2025-09-02 00:49:53 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,15 @@
 export const SITE_TITLE = 'Gränslöst Förtrollad';
 export const SITE_DESCRIPTION = 'Personligt, vardag & mörkare djup. GF som PWA-blogg.';
 
+// För sociala medier och SEO
+export const SITE_AUTHOR = 'Conri Turesson';
+export const SITE_URL = 'https://granslost-fortrollad.vercel.app';
+export const SITE_LANGUAGE = 'sv-SE';
+export const SITE_THEME_COLOR = '#8B0000';
+
+// Sociala länkar (kan användas i Footer/Header)
+export const SOCIAL_LINKS = {
+  github: 'https://github.com/RobynT98/granslost-fortrollad',
+  mastodon: 'https://mastodon.social/@dittkonto', // byt ut om du har eget
+  twitter: 'https://twitter.com/dittkonto'
+};
